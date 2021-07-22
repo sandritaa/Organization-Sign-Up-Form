758b5033458a14d1ab9484a2e1362b4791db0d59
sandrita <sandra@Sandras-Air.attlocal.net>
2021-07-22 16:34:57 -0700

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+<footer>
+  RedSol™ 
+</footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  
   }
 }
+
 </script>
 
 <style>
