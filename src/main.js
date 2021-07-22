3c794462f956c3c679dd853a9bfcd91813ece9f5
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-07-22 16:33:13 -0700

    css files

--- src/main.js
+++ src/main.js
@@ -3,11 +3,20 @@ import App from './App.vue'
 import store from './store'
 import PrimeVue from 'primevue/config';
 
+/**
+ *      CSS styles
+ */
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
 const app= createApp(App)
 /**************************
  *      Dependencies
  **************************/
 app.use(store).use(PrimeVue)
+
+
 
 app.mount('#app')
 
