50c9d2784ccf83a66394f75f40c0c1b43e53b674
sandrita <sandra@Sandras-Air.attlocal.net>
2021-07-22 17:56:27 -0700

    Modify css

--- src/main.js
+++ src/main.js
@@ -10,10 +10,8 @@ import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
 
-//copmment
 const app= createApp(App)
 
-// This is my comment
 /**************************
  *      Dependencies
  **************************/
