12229012c484712bb8a67d919f904bdc8e2a2071
sandrita <sandra@Sandras-Air.attlocal.net>
2021-07-22 17:14:41 -0700

    Fix structure

--- src/main.js
+++ src/main.js
@@ -3,24 +3,16 @@ import App from './App.vue'
 import store from './store'
 import PrimeVue from 'primevue/config';
 
-
 /**
  *      CSS styles
  */
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
-import 'primeflex/primeflex.css';
-import 'primeflex/src/_variables.scss';
-import 'primeflex/src/_grid.scss';
-import 'primeflex/src/_formlayout.scss';
-import 'primeflex/src/_display.scss';
-import 'primeflex/src/_text.scss';
-import 'primeflex/src/flexbox/_flexbox.scss';
-import 'primeflex/src/_spacing.scss';
-import 'primeflex/src/_elevation.scss';
 
 const app= createApp(App)
+
+
 /**************************
  *      Dependencies
  **************************/
@@ -29,5 +21,3 @@ app.use(store).use(PrimeVue)
 
 
 app.mount('#app')
-
-//poe
