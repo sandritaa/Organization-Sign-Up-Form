9783c20d8067ca868866a7ca9b8eb08604abeade
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-07-22 17:13:10 -0700

    init

--- src/main.js
+++ src/main.js
@@ -10,15 +10,7 @@ import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
-import 'primeflex/primeflex.css';
-import 'primeflex/src/_variables.scss';
-import 'primeflex/src/_grid.scss';
-import 'primeflex/src/_formlayout.scss';
-import 'primeflex/src/_display.scss';
-import 'primeflex/src/_text.scss';
-import 'primeflex/src/flexbox/_flexbox.scss';
-import 'primeflex/src/_spacing.scss';
-import 'primeflex/src/_elevation.scss';
+
 
 const app= createApp(App)
 /**************************
