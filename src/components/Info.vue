8b2442ce44a967aad38737c05d07e31e98acc7a9
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-07-28 16:26:01 -0700

    init

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,7 +3,7 @@
     <div class="card shadow-xl rounded-xl p-5 mx-auto">
         <div class="sm:grid grid-cols-2 p-5 justify-items-center">
 
-            <h3 class="text-center text-xl mb-6">Basic Information</h3>
+            <h3 class="text-center text-xl mb-6 col-span-2">Basic Information</h3>
 
             <!-- Organization name -->
             <div class=" w-full p-field p-col-16 p-md-4 col-span-2">
@@ -12,8 +12,6 @@
                     <label for="inputtext">Organization Name</label>
                 </span>
             </div>
-        
-        
 
             <!-- Name user input -->
             <div class="p-field p-col-12 p-md-4 w-full ">
@@ -54,10 +52,12 @@
 
             <div class="p-field p-col-12 p-md-4 col-span-2 w-full">
                 <span class="p-float-label mx-5">
-                    <InputText class="w-full" id="autocomplete" type="text" v-model="street" />
-                    <label for="inputtext">Street</label>
+                    <InputText class="w-full" id="autocomplete" type="text" v-model="address" placeholder=""/>
+                    <label for="inputtext">Address</label>
                 </span>
             </div>
+
+            <Button @click="print">print</Button>
 
         </div>
     </div> 
@@ -86,59 +86,25 @@ export default {
             email:null,
 
             //Location
-            street: null,
-            country: null,
-            city: null,
-            state: null,
-            zip: null,
+            address: null
 
-            // Variable contaiun a list of elements needed to display in the dropdown menu
-            allCountries:null,
-            states:null,
-            cities:null,
-
-            items:null,
         }
     },
     mounted() {
         const autocomplete = new google.maps.places.Autocomplete(
             document.getElementById("autocomplete")
         );
+
+        return autocomplete
     },
-
-
-    //add components 
     components: {InputText,Textarea,Dropdown,InputNumber},
-
-    //step1: get countries from API
-    async created(){
-        this.allCountries = await Country.getAllCountries()
-    },
-
     methods:{
-        async getState(){
-            this.city = null;
+        print(){
 
-            if(this.country){
-                this.states = await State.getStatesOfCountry(this.country.isoCode)
-            }
-        },
-        async getCity(){
-            if(this.state){
-
-                this.cities = await City.getCitiesOfState(this.country.isoCode, this.state.isoCode)
-            }
-        },
-        resetStateCity(){
-            this.state = null;
-            this.city = null;
-        },
-        setPlace(){
-
+            console.log(autocomplete.getPlace())
+            console.log(this.address)
         }
-        
     }
-
 }
 </script>
 
